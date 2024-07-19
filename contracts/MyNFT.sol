99076087c5c113193ba35a6ba79e9a7f1c1881c8
penn <penn201500@gmail.com>
2024-07-19 18:10:28 +0800

    fix MyNFT.sol and deploy it to holesky testnet

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -1,26 +1,27 @@
 //Contract based on [https://docs.openzeppelin.com/contracts/3.x/erc721](https://docs.openzeppelin.com/contracts/3.x/erc721)
-   // SPDX-License-Identifier: MIT
-   pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-   import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-   import "@openzeppelin/contracts/access/Ownable.sol";
-   import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-   contract MyNFT is ERC721URIStorage, Ownable {
-       uint256 private _tokenIds;
+contract MyNFT is ERC721URIStorage, Ownable {
+    uint256 private _tokenIds;
 
-       constructor() ERC721("MyNFT", "NFT") Ownable(msg.sender) {}
+    // Pass the owner's address to the Ownable constructor
+    constructor(address initialOwner) ERC721("MyNFT", "NFT") Ownable(initialOwner) {}
 
-       function mintNFT(address recipient, string memory tokenURI)
-           public onlyOwner
-           returns (uint256)
-       {
-           _tokenIds += 1;
+    function mintNFT(address recipient, string memory tokenURI)
+        public onlyOwner
+        returns (uint256)
+    {
+        _tokenIds += 1;
 
-           uint256 newItemId = _tokenIds;
-           _mint(recipient, newItemId);
-           _setTokenURI(newItemId, tokenURI);
+        uint256 newItemId = _tokenIds;
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
 
-           return newItemId;
-       }
-   }
+        return newItemId;
+    }
+}
---
+++ scripts/deploy.js
@@ -0,0 +1,19 @@
+// scripts/deploy.js
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const MyNFT = await ethers.getContractFactory("MyNFT");
+  const myNFT = await MyNFT.deploy(deployer.address);
+
+  await myNFT.deployed();
+  console.log("MyNFT deployed to:", myNFT.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
