2b892010e60819087bd9ad38e30dfaf95534983e
penn <penn201500@gmail.com>
2024-07-19 17:39:30 +0800

    npx hardhat compile smart contract

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -3,27 +3,24 @@
    pragma solidity ^0.8.0;
 
    import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-   import "@openzeppelin/contracts/utils/Counters.sol";
    import "@openzeppelin/contracts/access/Ownable.sol";
    import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
    contract MyNFT is ERC721URIStorage, Ownable {
-       using Counters for Counters.Counter;
-       Counters.Counter private _tokenIds;
+       uint256 private _tokenIds;
 
-       constructor() ERC721("MyNFT", "NFT") {}
+       constructor() ERC721("MyNFT", "NFT") Ownable(msg.sender) {}
 
        function mintNFT(address recipient, string memory tokenURI)
            public onlyOwner
            returns (uint256)
        {
-           _tokenIds.increment();
+           _tokenIds += 1;
 
-           uint256 newItemId = _tokenIds.current();
+           uint256 newItemId = _tokenIds;
            _mint(recipient, newItemId);
            _setTokenURI(newItemId, tokenURI);
 
            return newItemId;
        }
    }
-
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,15 @@
 /** @type import('hardhat/config').HardhatUserConfig */
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+const {API_URL, PRIVATE_KEY} = process.env;
 module.exports = {
   solidity: "0.8.24",
+  defaultNetwork: "holesky",
+  networks: {
+    hardhat: {},
+    holesky: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  }
 };
